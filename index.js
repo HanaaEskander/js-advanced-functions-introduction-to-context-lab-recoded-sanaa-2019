ec89578a5a31f60467a3d179a82918f656e5ba27
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-04 09:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,87 +1,86 @@
-// Your code here
-function createEmployeeRecord(sourceArray) {
-    let obj = {
-        firstName: sourceArray[0],
-        familyName: sourceArray[1],
-        title: sourceArray[2],
-        payPerHour: sourceArray[3],
-        timeInEvents: [],
-        timeOutEvents: []
+function createEmployeeRecord(arr){
+  let empObj = {
+    firstName:arr[0],
+    familyName:arr[1],
+    title:arr[2],
+    payPerHour:arr[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+  }
+  return empObj;
+}
+
+function createEmployees(arr){
+  return arr.map(function(e){
+    return {
+      firstName:e[0],
+      familyName:e[1],
+      title:e[2],
+      payPerHour:e[3],
+      timeInEvents:[],
+      timeOutEvents:[]
     }
-    return obj;
+  });
 }
 
-function createEmployees(sourceArray) {
-    return sourceArray.map(function (data) {
-        return {
-            firstName: data[0],
-            familyName: data[1],
-            title: data[2],
-            payPerHour: data[3],
-            timeInEvents: [],
-            timeOutEvents: []
-        }
-    });
+function createTimeInEvent(obj,time){
+  let newObj = Object.assign({},obj);
+  let timeParts = time.split(' ');
+  let timeInObj = {
+    type:"TimeIn",
+    date: timeParts[0],
+    hour: parseInt(timeParts[1])
+  }
+  newObj.timeInEvents.push(timeInObj);
+  return newObj;
 }
 
-function createTimeInEvent(obj, time) {
-    let obj1 = Object.assign({}, obj1);
-    let timeParts = time.split(' ');
-    let timeInObj = {
-        type: "TimeIn",
-        date: timeParts[0],
-        hour: parseInt(timeParts[1])
-    }
-    obj1.timeInEvents.push(timeInObj);
-    return obj1;
+function createTimeOutEvent(obj,time){
+  let newObj = Object.assign({},obj);
+  let timeParts = time.split(' ');
+  let timeInObj = {
+    type:"TimeOut",
+    date: timeParts[0],
+    hour: parseInt(timeParts[1])
+  }
+  newObj.timeOutEvents.push(timeInObj);
+  return newObj;
 }
 
-function createTimeOutEvent(obj, time) {
-    let obj2 = Object.assign({}, obj2);
-    let timeParts = time.split(' ');
-    let timeInObj = {
-        type: "TimeOut",
-        date: timeParts[0],
-        hour: parseInt(timeParts[1])
-    }
-    obj2.timeOutEvents.push(timeInObj);
-    return obj2;
-}
-
-function hoursWorkedOnDate(record, date) {
-    let start = record.timeInEvents.find(emp => emp.date === date);
-    let end = record.timeOutEvents.find(emp => emp.date === date);
-    let startH = start.hour.toString();
-    let endH = end.hour.toString();
-    let startHo = startHo.length === 3 ? parseInt(startHo.substring(0, 1)) : parseInt(startHo.substring(0, 2));
-    let endHo = endHo.length === 3 ? parseInt(endHo.substring(0, 1)) : parseInt(endHo.substring(0, 2));
-    let total = endHo - startH;
-    return total;
+function hoursWorkedOnDate(record,date){
+  let startTime = record.timeInEvents.find(ele=>ele.date === date);
+  let EndTime = record.timeOutEvents.find(ele=>ele.date === date);
+  let startHour = startTime.hour.toString();
+  let endHour = EndTime.hour.toString();
+  let startH = startHour.length === 3 ? parseInt(startHour.substring(0,1)) :parseInt(startHour.substring(0,2));
+  let endH = endHour.length === 3 ? parseInt(endHour.substring(0,1)) : parseInt(endHour.substring(0,2));
+  let sum = endH - startH;
+  return sum;
 
 }
 
-function wagesEarnedOnDate(record, date) {
-    let total = hoursWorkedOnDate(record, date);
-    return total * record.payPerHour;
+function wagesEarnedOnDate(record,date){
+  let sum = hoursWorkedOnDate(record,date);
+  return sum * record.payPerHour;
 }
 
-function allWagesFor(record) {
-    let total = [];
-    for (let i = 0; i < record.timeInEvents.length; i++) {
-        total.push(wagesEarnedOnDate(record, record.timeInEvents[i].date));
-    }
-    let finalW = total.reduce((n, l) => n + l, 0);
-    return finalW;
+function allWagesFor(record){
+  let sum = [];
+  for(let i = 0; i < record.timeInEvents.length; i++){
+    sum.push(wagesEarnedOnDate(record,record.timeInEvents[i].date));
+  }
+  let final = sum.reduce((acc, cur) => acc + cur, 0);
+  return final;
 }
 
-function calculatePayroll(employeeArray) {
-    return employeeArray.reduce((a, h) => a + allWagesFor(h), 0);
+function calculatePayroll(empArr){
+  return empArr.reduce((m, e) => m + allWagesFor(e), 0);
 }
 
-function createEmployeeRecords(resume) {
-    return resume.map(item => createEmployeeRecord(item));
+function createEmployeeRecords(cvs){
+  return cvs.map(item=>createEmployeeRecord(item));
 }
 
-function findEmployeeByFirstName(employeeArray, employeeFun) {
-    return employeeArray.find(emp => emp.firstName === employeeFun);
+function findEmployeebyFirstName(empArr,empFN){
+  return empArr.find(emp=>emp.firstName === empFN);
 }
